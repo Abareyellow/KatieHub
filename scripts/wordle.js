eaecaf7e0e8400d0e9419ab2e6a39a525dc1dd0e
Abareyellow <30733981+Abareyellow@users.noreply.github.com>
2023-08-01 12:49:29 +0000

    Got a master list of words.

--- scripts/wordle.js
+++ scripts/wordle.js
@@ -1,1 +1,14 @@
-let start = $(.enter);
+let enter = $('.enter');
+console.log(enter);
+
+var fileContent = $.ajax({type: "GET", url: "https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/6bfa15d263d6d5b63840a8e5b64e04b382fdb079/valid-wordle-words.txt", async: false}, ).responseText;
+
+let masterList = fileContent.split('\n');
+console.log(masterList)
+
+
+//fileContent = jQuery.get('https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/6bfa15d263d6d5b63840a8e5b64e04b382fdb079/valid-wordle-words.txt').responseText;
+
+//console.log(fileContent)
+//console.log(file)
+//var msg = $.ajax({type: "GET", url: "my_script.php", async: false}).resp
