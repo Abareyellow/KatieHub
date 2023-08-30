1a18b9a917fa93a78b734848df79a2dbeee9314d
Abareyellow <30733981+Abareyellow@users.noreply.github.com>
2023-08-30 01:38:50 +0000

    Start my javascript for this file. Got the input values.

---
+++ scripts/pledge-flag.js
@@ -0,0 +1,13 @@
+let $button = $(".submit");
+
+$button.on("click", submitResults);
+
+function submitResults() {
+  let $firstname = $("input[name='firstname']").val();
+  let $pledge = $("input[name='pledge']").val();
+  let $background = $("input[name='background']").val();
+  let $background2 = $("input[name='background2']").val();
+  let $font = $("input[name='font']").val();
+
+  
+}
