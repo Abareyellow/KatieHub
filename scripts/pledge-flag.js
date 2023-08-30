471f0e2666a7681f02d6c257d8cd87c64dfaf6b4
Abareyellow <30733981+Abareyellow@users.noreply.github.com>
2023-08-30 23:19:32 +0000

    The first start to making the flag.
    There needs more design with the flag.

--- scripts/pledge-flag.js
+++ scripts/pledge-flag.js
@@ -9,5 +9,19 @@ function submitResults() {
   let $background2 = $("input[name='background2']").val();
   let $font = $("input[name='font']").val();
 
-  
+  maketheCard($firstname, $pledge, $background, $background2, $font);
+}
+
+function maketheCard(name, pledge, background, foreground, font) {
+  let result = $('.results');
+  let pledge_card = $('.pledge-card')
+  let firstname = $('.pledge-card h1')
+  let pledge_para = $('.pledge-card p')
+
+  result.removeClass('hidden')
+  pledge_card.css('background-color', background)
+  firstname.text(name);
+  firstname.css('color', font, 'background-color', foreground)
+  pledge_para.text(pledge)
+  pledge_para.css('color', font, 'background-color', foreground)
 }
