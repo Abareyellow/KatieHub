75c78e77ad84ad17818eca3abc3962256ccf2688
Abareyellow <30733981+Abareyellow@users.noreply.github.com>
2023-10-18 16:27:27 +0000

    still trying something

--- scripts/markdown.js
+++ scripts/markdown.js
@@ -1,5 +1,7 @@
 import React from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom";
+import marked from "https://cdn.skypack.dev/marked";
+import Prism from "https://cdn.skypack.dev/prismjs";
 
 marked.setOptions({
   breaks: true,
@@ -15,63 +17,36 @@ renderer.link = function(href, title, text) {
 
 const defaultContent = `# Welcome to my React Markdown Previewer!
 
-## This is a sub-heading...
-### And here's some other cool stuff:
+...
 
-Heres some code, \`<div></div>\`, between 2 backticks.
+![Pikachu](https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-blue-version/8/89/Pikachu.jpg)`;
 
-You can also make text **bold**... whoa!
-Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
+const Editor = ({ content, handleTextareaChange }) => (
+  <textarea id="editor" value={content} onChange={handleTextareaChange} />
+);
 
-There's also [links](https://www.freecodecamp.org), and
-> Block Quotes!
-
-And if you want to get really crazy, even tables:
-
-Wild Header | Crazy Header | Another Header?
------------- | ------------- | -------------
-Your content can | be here, and it | can be here....
-And here. | Okay. | I think we get it.
-
-- And of course there are lists.
-  - Some are bulleted.
-     - With different indentation levels.
-        - That look like this.
-
-\`\`\`
-const multipleLineCode = (param) => {
-    if(param) {
-        return param
-    }
-}
-\`\`\`
-
-1. And there are numbered lists too.
-1. Use just 1s if you want!
-1. And last but not least, let's not forget embedded images:
-
-![Pikachu](https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-blue-version/8/89/Pikachu.jpg)`
-
-const Editor = ({ content, handleTextareaChange }) => <textarea id="editor" value={content} onChange={handleTextareaChange}/>
-
-const Previewer = ({content}) => <div id="preview" dangerouslySetInnerHTML={{
-    __html: marked(content, { renderer: renderer })
-  }} />
+const Previewer = ({ content }) => (
+  <div
+    id="preview"
+    dangerouslySetInnerHTML={{
+      __html: marked(content, { renderer: renderer }),
+    }}
+  />
+);
 
 const App = () => {
-  const [content, setContent] = React.useState(defaultContent)
-
+  const [content, setContent] = React.useState(defaultContent);
+  
   const handleTextareaChange = (event) => {
-    setContent(event.target.value)
-  }
+    setContent(event.target.value);
+  };
+
   return (
     <div className="main">
-      <Editor content={content} handleTextareaChange={handleTextareaChange}/>
+      <Editor content={content} handleTextareaChange={handleTextareaChange} />
       <Previewer content={content} />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.querySelector("#app"))
+ReactDOM.render(<App />, document.querySelector("app"));
