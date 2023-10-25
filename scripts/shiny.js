6614c849d24d9cf4a97f6bd9d0db011bdec95db6
Abareyellow <30733981+Abareyellow@users.noreply.github.com>
2023-10-25 18:43:04 +0000

    Making changes to my Pokemon gallery.

--- scripts/shiny.js
+++ scripts/shiny.js
@@ -1,8 +1,5 @@
-function openMethod(methodName) {
-  var x = document.getElementsByClassName("arrange");
-  console.log(x)
-  for (let i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  document.getElementById(methodName).style.display = "inline";
+$('.go').on("click", displayGo)
+
+function displayGo() {
+  
 }
