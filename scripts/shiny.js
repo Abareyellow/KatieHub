f67f24a4b05d525811ffc7e8a0eafb57eb150de8
Abareyellow <30733981+Abareyellow@users.noreply.github.com>
2023-10-27 13:41:05 +0000

    Everything works!!! New photos will be coming soon!!

--- scripts/shiny.js
+++ scripts/shiny.js
@@ -1,5 +1,28 @@
+let go = $('#go')
+let arceus = $('#arceus')
+let violet = $('#violet')
+
 $('.go').on("click", displayGo)
+$('.arceus').on("click", displayArceus)
+$('.violet').on("click", displayViolet)
 
 function displayGo() {
-  
+  console.log("Here")
+  go.removeClass('hidden')
+  arceus.addClass('hidden')
+  violet.addClass('hidden')
+}
+
+function displayArceus() {
+  console.log("Here")
+  go.addClass('hidden')
+  arceus.removeClass('hidden')
+  violet.addClass('hidden')
+}
+
+function displayViolet() {
+  console.log("Here")
+  go.addClass('hidden')
+  arceus.addClass('hidden')
+  violet.removeClass('hidden')
 }
